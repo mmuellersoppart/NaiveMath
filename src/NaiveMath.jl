930ffbdf6404c9cf8d2e6f42d1bf681d6b72a0ba
mmuellersoppart <mmuellersoppart@gmail.com>
2022-12-28 11:22:45 -0600

    surface primefactorization

--- src/NaiveMath.jl
+++ src/NaiveMath.jl
@@ -1,23 +1,45 @@
 module NaiveMath
     module Numerics
         module Primes
-            export isprime
+            export isprime, primefactorization
 
-            function isprime()
-                "hi"
+            function isprime(NN)
+                for i in 2:(NN - 1)
+                    mod(NN, i) == 0 && return false
+                end
+                return true
             end
-        end
-        using .Primes
-        export isprime
-        
-        
 
-    end
+            function primefactorization(NN)
+
+                isprime(NN) && return [NN]
+
+                primes = []
+                composites = [NN]
+                while(!isempty(composites))
+                    composite = pop!(composites)
+
+                    for i in 2:composite
+                        if mod(composite, i) == 0
+                            isprime(i) ? push!(primes, i) : push!(composites, i)
+
+                            divisor = composite ÷ i
+                            isprime(divisor) ? push!(primes, divisor) : push!(composites, divisor)
+                            break
+                        end
+                    end
+                end
+                return primes
+            end
+
+        end # module Primes
+
+        using .Primes
+        export isprime, primefactorization
+
+    end # module Numerics
 
     using .Numerics
-    export isprime
-
-    
-greet() = print("Hello World!")
+    export Numerics
 
 end; # module NaiveMath
